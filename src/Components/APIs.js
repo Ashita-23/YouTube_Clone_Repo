0fb1c85889d32aa81cdc068dabd3d5a4b184aa2b
Akash <akashbytes@gmail.com>
2023-06-02 22:33:54 +0530

    file for You tube Apis

--- src/Components/APIs.js
+++ src/Components/APIs.js
@@ -1,4 +1,4 @@
 
-const YouTube_API_Key = "AIzaSyDXIaZrH87YtS0JHWxrHRxlEDCVR1KkwYs" ;
+const YouTube_API_UserKey = "AIzaSyDXIaZrH87YtS0JHWxrHRxlEDCVR1KkwYs" ;
 
-export const YouTube_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="+YouTube_API_Key;
+export const YouTube_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="+YouTube_API_UserKey;
