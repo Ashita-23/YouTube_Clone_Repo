e3a28a43ae96ec3abd45dab671f98b9d911fbcbb
Akash <akashbytes@gmail.com>
2023-09-08 17:04:06 +0530

    created utils

---
+++ src/Utils/UTAPIs.js
@@ -0,0 +1,5 @@
+
+const YouTube_API_UserKey = "AIzaSyB0BYF-z7QkTKBfNS25fwTBLR9r18FBsLs" ;
+
+
+export const YouTube_API =  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+YouTube_API_UserKey ;
