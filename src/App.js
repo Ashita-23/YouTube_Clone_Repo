fe39075b8d8e1e004184e0e221a0c539ad9859a3
Akash <akashbytes@gmail.com>
2023-09-08 17:07:05 +0530

    old code edited and new components are called

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 
-import './App.css';
+import { useEffect } from "react";
+import "./App.css"
+import { YouTube_API } from "./Utils/UTAPIs";
+import Navigation from "./Components/Nav";
 
 function App() {
+  console.log(fetch(YouTube_API) ,  "YAPIFEX")
+
+  useEffect(()=>{
+    getUtubeData()
+  },[])
+
+ const  getUtubeData = async ()=> {
+   
+  const Utube_API = await fetch(YouTube_API);
+  const Json = await Utube_API.json()
+
+  console.log(Json)
+  }
+
+
+
   return (
-  <div>
-    You tube Clone
-  </div>
+
+
+ 
+  <Navigation></Navigation>
+
   );
 }
 
