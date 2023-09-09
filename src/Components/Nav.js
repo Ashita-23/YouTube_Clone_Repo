725116015289a8cb295c4339f91d7996e21521e9
Akash <akashbytes@gmail.com>
2023-09-09 16:21:43 +0530

    code edited

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -7,7 +7,7 @@ const Navigation = ()=>{
             <div  className="" ><img src={YT_LOGO} alt="YT_logo" className="w-32"/></div>
             <div  className=" w-[50rem] flex">
             <input type="search" className="border px-3 border-black-400 rounded-l-full w-[40rem] focus:outline outline-blue-400" />
-            <span  className="text-xl px-3 py-2  border border-black-200 rounded-r-full mr-2 bg-gray-100 hover:bg-gray-300" ><i class="fa-solid fa-magnifying-glass"></i></span>
+            <span  className="text-xl px-3 py-2  border border-black-200 rounded-r-full mr-2 bg-gray-100 hover:bg-gray-300" ><i className="fa-solid fa-magnifying-glass"></i></span>
             <span  className="text-xl px-4 py-2 border border-black-200 rounded-full mx-2 bg-gray-100 hover:bg-gray-300"><i className="fa-solid fa-microphone hover:bg-black-200"></i></span></div>
             <div  className="w-[10rem] flex justify-around">
             <span className="text-2xl  px-3 py-2 rounded-full bg-gray-50 hover:bg-gray-200"><i className="fa-regular fa-square-plus"></i></span>
