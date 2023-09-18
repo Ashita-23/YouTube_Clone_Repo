17f06b02fb3f2f421b9570128ae198484d9f2418
Akash <akashbytes@gmail.com>
2023-09-18 14:21:07 +0530

    added some more text

--- src/Components/BodyComponents/SideBar.js
+++ src/Components/BodyComponents/SideBar.js
@@ -3,7 +3,30 @@
 const SideBar = ()=>{
     return(<>
            {/* side bar */}
-           <div className=" border border-gray-400 w-[10%] h-[90%]">
+           <div className=" border border-gray-400 w-[10%] h-[90%] mt-16 ">
+            <ul>
+                <li>Home</li>
+                <li>Shorts</li>
+                <li>Subscriptions</li>
+            </ul>
+            <ul>
+                <li>Library</li>
+                <li>History</li>
+                <li>Your Videos</li>
+                <li>watch Later</li>
+                <li>Liked videos</li>
+            </ul>
+            <ul>
+                <li>Trending</li>
+                <li>Shopping</li>
+                <li>Music</li>
+                <li>Films</li>
+                <li>Gaming</li>
+                <li>News</li>
+                <li>Sport</li>
+                <li>Learing</li>
+                <li>Fashion & Beauty</li>
+            </ul>
             <ul>
                 <li>Home</li>
                 <li>Shorts</li>
