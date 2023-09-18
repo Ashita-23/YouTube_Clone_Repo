915db584d137233e06293636d7033e211d7aff81
Akash <akashbytes@gmail.com>
2023-09-18 17:08:37 +0530

    videocard commponent

---
+++ src/Components/BodyComponents/VideoCard.js
@@ -0,0 +1,16 @@
+
+const VideoCard = ({data})=>{
+    return( <div className=" rounded-2xl p-1 w-[20rem] m-2" >
+    <img src={data?.snippet?.thumbnails?.medium?.url} alt="thumbnail" className="rounded-2xl "/>
+    <div className="flex justify-evenly items-start">
+   <span className="text-2xl  px-3 py-2 m-1 rounded-full bg-gray-100 hover:bg-gray-200"><i className="fa-regular fa-circle-user"></i></span>
+    <div className="overflow-hidden">   
+    <p className="text-sm truncate font-medium ">{data?.snippet?.title}</p>
+       <p className="text-md">{data?.snippet?.channelTitle}</p>
+       <p className="text-xs ">{data?.statistics?.viewCount} views</p></div>
+
+    </div>
+   </div>)
+}
+
+export default VideoCard
