7b4c2a3bc0d242793efb05021166cedbba659923
Akash <akashbytes@gmail.com>
2023-09-21 18:08:25 +0530

    search suggetion api called

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,20 +1,59 @@
 
+import { useEffect, useState } from "react"
 import YT_LOGO from  "../assets/ytlogo.png"
+import {YT_search_API} from "../Utils/UTAPIs"
+
+
 
 
 const Navigation = ()=>{
-    return(<div className="flex justify-around bg-white p-2 drop-shadow-md fixed w-[100%]" >
+
+const [search,setSearch] = useState("")
+console.log(search)
+const [Suggetions , setSuggetions] = useState([""])
+const [ShowSuggetions , setShowSuggetions] = useState(false)
+
+useEffect(()=>{
+   const search_timer = setTimeout(()=>getSearch(),100) 
+
+   return()=>{
+    clearTimeout(search_timer)
+   };
+},[search])
+
+const getSearch = async () =>{
+    const Search_API  = await fetch(YT_search_API+search)
+    const Search_API_Json = await Search_API.json()
+    // console.log(Search_API_Json , "sAJ")
+    setSuggetions(Search_API_Json[1])
+}
+    return(<div className="flex justify-around bg-white p-2 fixed w-[100%] z-10" >
             <div className=""><span className="text-2xl"><i className="fa-solid fa-bars"></i></span></div>
             <div  className="" ><img src={YT_LOGO} alt="YT_logo" className="w-32"/></div>
+            {/* {________---------- SEARCH INPUT BOX        -----------_______________} */}
             <div  className=" w-[50rem] flex">
-            <input type="search" className="border px-3 border-black-400 rounded-l-full w-[40rem] focus:outline outline-blue-400" />
+            
+            <input type="search" value={search} onChange={(e)=>setSearch(e.target.value)} placeholder="Search"
+             className="border px-4 border-black-400 rounded-l-full w-[40rem] focus:outline outline-blue-400" 
+                onFocus={()=>setShowSuggetions(true)}
+                onBlur={()=>setShowSuggetions(false)}
+             />
+
             <span  className="text-xl px-3 py-2  border border-black-200 rounded-r-full mr-2 bg-gray-100 hover:bg-gray-300" ><i className="fa-solid fa-magnifying-glass"></i></span>
-            <span  className="text-xl px-4 py-2 border border-black-200 rounded-full mx-2 bg-gray-100 hover:bg-gray-300"><i className="fa-solid fa-microphone hover:bg-black-200"></i></span></div>
+            <span  className="text-xl px-4 py-2 border border-black-200 rounded-full mx-2 bg-gray-100 hover:bg-gray-300"><i className="fa-solid fa-microphone hover:bg-black-200"></i></span>
+            </div>
+           {ShowSuggetions && <div className="absolute mt-12 ml-6 bg-gray-200 w-[40rem] rounded-lg pt-2 pb-1 shadow-lg" >
+                <ul> 
+                  {Suggetions.map((Suge_data)=> <li className="text-base font-bold px-3 py-1 bg-gray-200 hover:bg-gray-300"><i className="fa-solid fa-magnifying-glass text-sm px-1"></i>{Suge_data}</li>)}
+                </ul>
+            </div>}
+            
             <div  className="w-[10rem] flex justify-around">
             <span className="text-2xl  px-3 py-2 rounded-full bg-gray-50 hover:bg-gray-200"><i className="fa-regular fa-square-plus"></i></span>
             <span className="text-2xl  px-3 py-2 rounded-full bg-gray-50 hover:bg-gray-200"><i className="fa-regular fa-bell"></i></span>
             <span className="text-2xl  px-3 py-2  rounded-full bg-gray-50 hover:bg-gray-200 "><i className="fa-regular fa-circle-user"></i></span>
             </div>
+           
         </div>)
 }
 
