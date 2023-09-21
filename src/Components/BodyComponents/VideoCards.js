bf4c825b03747f683ffd57c2c622da965905582e
Akash <akashbytes@gmail.com>
2023-09-21 18:09:45 +0530

    upto date

--- src/Components/BodyComponents/VideoCards.js
+++ src/Components/BodyComponents/VideoCards.js
@@ -21,25 +21,12 @@ const VideoCards = ()=>{
     // console.log(Json)
     }
 
-
+ const Button_List = ["All","Food","Live","New Song" , "Dogs" , "criket" , "Footbal" , "Dance" ,"book" , "News","Food","Live","New Song" , "Dogs" , "criket" , "Footbal" , "Dance" ,"book" , "News"]
 
 
    return( <div className="border border-blue-800 w-[100%] px-0 py-0 mt-14 h-[100%]">
-    <div className="flex w-[100%] bg-white justify-evenly fixed py-3 drop-shadow-sm overflow-hidden">
-            <button className="bg-gray-300  text-md rounded-md px-3 text-black-300 py-1 ">All</button>
-            <button className="bg-gray-300  text-md rounded-md px-3 text-black-300 py-1 ">India</button>
-            <button className="bg-gray-300  text-md rounded-md px-3 text-black-300 py-1 ">Dance</button>
-            <button className="bg-gray-300  text-md rounded-md px-3 text-black-300 py-1 ">Food</button>
-            <button className="bg-gray-300  text-md rounded-md px-3 text-black-300 py-1 ">Reacipe</button>
-            <button className="bg-gray-300  text-md rounded-md px-3 text-black-300 py-1 ">New Song</button>
-            <button className="bg-gray-300  text-md rounded-md px-3 text-black-300 py-1 ">Vlogs</button>
-            <button className="bg-gray-300  text-md rounded-md px-3 text-black-300 py-1 ">Dogs</button>
-            <button className="bg-gray-300  text-md rounded-md px-3 text-black-300 py-1 ">News</button>
-            <button className="bg-gray-300  text-md rounded-md px-3 text-black-300 py-1 ">buttonve</button>
-            <button className="bg-gray-300  text-md rounded-md px-3 text-black-300 py-1 ">Vlogs</button>
-            <button className="bg-gray-300  text-md rounded-md px-3 text-black-300 py-1 ">Dogs</button>
-            <button className="bg-gray-300  text-md rounded-md px-3 text-black-300 py-1 ">News</button>
-            <button className="bg-gray-300  text-md rounded-md px-3 text-black-300 py-1 ">buttonve</button>
+    <div className="flex w-[100%] justify-evenly bg-white fixed py-3 drop-shadow-sm overflow-scroll ">
+            {Button_List.map((Btn_text)=><button className="bg-gray-300  text-md rounded-md px-3 text-black-300 py-1 ">{Btn_text}</button>) }
        
     </div>
 <div className="flex flex-wrap justify-evenly border pt-2 border-red-400 w-[100%] h-[70rem] scroll-smooth overflow-scroll mt-12">
