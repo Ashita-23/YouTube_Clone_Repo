be623982374b8c361b3404ee8bc79d5629371ef5
Akash <akashbytes@gmail.com>
2023-09-21 18:08:55 +0530

    You tune search suggestion api id added

--- src/Utils/UTAPIs.js
+++ src/Utils/UTAPIs.js
@@ -4,6 +4,6 @@ const YouTube_API_UserKey = "AIzaSyB0BYF-z7QkTKBfNS25fwTBLR9r18FBsLs" ;
 
 export const YouTube_API =  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+YouTube_API_UserKey ;
 
-export const YouTube_ID_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc&key="+YouTube_API_UserKey ;
+// export const YouTube_ID_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc&key="+YouTube_API_UserKey ;
 
-export const UT_search_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=Query" ;
+export const YT_search_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=" ;
