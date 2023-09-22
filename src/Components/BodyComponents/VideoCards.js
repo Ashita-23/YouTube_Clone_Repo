9a2b568eea044693ca9e04290f72fd2c55bb2318
Akash <akashbytes@gmail.com>
2023-09-22 16:09:34 +0530

    scrollbar modified

--- src/Components/BodyComponents/VideoCards.js
+++ src/Components/BodyComponents/VideoCards.js
@@ -21,15 +21,15 @@ const VideoCards = ()=>{
     // console.log(Json)
     }
 
- const Button_List = ["All","Food","Live","New Song" , "Dogs" , "criket" , "Footbal" , "Dance" ,"book" , "News","Food","Live","New Song" , "Dogs" , "criket" , "Footbal" , "Dance" ,"book" , "News"]
+ const Button_List = ["All","Food","Live","New" , "Dogs" , "criket" , "Footbal" , "Dance" ,"book" , "News","Food","Live","New" , "Dogs" , "criket" , "Footbal" , "Dance" ,"book" , "News","Food","Live","New" , "Dogs" , "criket" , "Footbal" , "Dance" ,"book" , "News"]
 
 
-   return( <div className="border border-blue-800 w-[100%] px-0 py-0 mt-14 h-[100%]">
-    <div className="flex w-[100%] justify-evenly bg-white fixed py-3 drop-shadow-sm overflow-scroll ">
-            {Button_List.map((Btn_text)=><button className="bg-gray-300  text-md rounded-md px-3 text-black-300 py-1 ">{Btn_text}</button>) }
-       
+   return( <div className="border border-blue-800 w-[100%] px-0 py-0 mt-16 h-[100%]">
+    <div className="flex w-[91%] h-[5rem]   mt-[-2px] bg-white fixed py-3 drop-shadow-sm overflow-x-scroll scroll-smooth scrollbar-thin
+    scrollbar-w-thin scrollbar-rounded-full   scrollbar-thumb-gray-600 scrollbar-track-gray-200  snap-x">
+            {Button_List.map((Btn_text)=><button className="bg-gray-300 mx-2 text-md rounded-md px-3 text-black-300 py-1 ">{Btn_text}</button>) }
     </div>
-<div className="flex flex-wrap justify-evenly border pt-2 border-red-400 w-[100%] h-[70rem] scroll-smooth overflow-scroll mt-12">
+<div className="flex flex-wrap justify-start border pt-8 border-red-400 w-[100%] h-[70rem] scroll-smooth overflow-y-scroll scrollbar-hide snap-y mt-12">
 {UT_Data[0] && <AddVideoCard data={UT_Data[0]} ></AddVideoCard>}
 { UT_Data?.map((data)=>{return(<Link to={"/watch?v="+ data?.id} key={data?.id}><VideoCard data={data} > </VideoCard></Link>)})}
 </div>
@@ -39,3 +39,7 @@ const VideoCards = ()=>{
 }
 
 export default VideoCards
+
+
+
+
