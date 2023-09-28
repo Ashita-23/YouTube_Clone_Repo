4c1f5dcfa879c64d120c46f49908ba06a1083b2c
Akash <akashbytes@gmail.com>
2023-09-28 16:41:25 +0530

    searchCache slice are added

--- src/RStore/UStore.js
+++ src/RStore/UStore.js
@@ -1,8 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit"
 import NavToggle from "./ToggelSlice"
+import SearchCache from "./SearchCacheSlice"
 const AppStore = configureStore({
     reducer:{
      Toggle : NavToggle,
+     SearchCacheData : SearchCache,
     }
 })
 
