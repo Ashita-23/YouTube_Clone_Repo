771ddfd4fc6a91db77ace18bd7fedc4ad06278c7
Akash <akashbytes@gmail.com>
2023-09-28 15:36:57 +0530

    counter modified

--- src/Components/BodyComponents/VideoCards.js
+++ src/Components/BodyComponents/VideoCards.js
@@ -26,7 +26,7 @@ const VideoCards = ()=>{
 
    return( <div className="border border-blue-800 w-[100%] px-0 py-0 mt-16 h-[100%]">
     <div className="flex w-[91%] h-[5rem]   mt-[-2px] bg-white fixed py-3 drop-shadow-sm overflow-x-scroll scroll-smooth scrollbar-thin
-    scrollbar-w-thin scrollbar-rounded-full   scrollbar-thumb-gray-600 scrollbar-track-gray-200  snap-x">
+    scrollbar-w-thin scrollbar-rounded-full items-center scrollbar-thumb-gray-600 scrollbar-track-gray-200  snap-x">
             {Button_List.map((Btn_text)=><button className="bg-gray-300 mx-2 text-md rounded-md px-3 text-black-300 py-1 ">{Btn_text}</button>) }
     </div>
 <div className="flex flex-wrap justify-start border pt-8 border-red-400 w-[100%] h-[70rem] scroll-smooth overflow-y-scroll scrollbar-hide snap-y mt-12">
