709b451d14f8d6fff184292ed589d91a47343c92
Akash <akashbytes@gmail.com>
2023-10-03 19:22:04 +0530

    old logic is modified

--- src/RStore/UStore.js
+++ src/RStore/UStore.js
@@ -1,10 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit"
 import NavToggle from "./ToggelSlice"
 import SearchCache from "./SearchCacheSlice"
+import SearchItems from "./SearchItemSlice"
+import YouTubeData from "./YouTubeDataSlice"
 const AppStore = configureStore({
     reducer:{
      Toggle : NavToggle,
      SearchCacheData : SearchCache,
+     SearchText_FromBtns :SearchItems,
+     You_Tube_Data :YouTubeData,
     }
 })
 
