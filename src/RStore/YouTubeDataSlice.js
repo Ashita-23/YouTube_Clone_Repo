590286ffb4c5162eaf2698a5615833bbfe36e659
Akash <akashbytes@gmail.com>
2023-10-03 19:21:09 +0530

    created store for storen api data to filter items from different commponents

---
+++ src/RStore/YouTubeDataSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const YouTubeData = createSlice({
+    name : "UT_Data",
+    initialState:{
+       items : [],
+    },
+    reducers:{
+        Add_UT_Data :(state,action)=>{
+            state.items.push(action.payload)
+        }
+    }
+})
+
+export const {Add_UT_Data} = YouTubeData.actions
+export default YouTubeData.reducer
