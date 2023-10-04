7231426164fd72aa3da6cba3427c67c7fc9e59c2
Akash <akashbytes@gmail.com>
2023-10-04 15:59:30 +0530

    created api for random name and text generator

--- src/Utils/UTAPIs.js
+++ src/Utils/UTAPIs.js
@@ -7,7 +7,11 @@ export const YouTube_API =  "https://youtube.googleapis.com/youtube/v3/videos?pa
 // export const YouTube_ID_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc&key="+YouTube_API_UserKey ;
 
 export const YT_search_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=" ;
+export const FirstName = 
+[ "Aabha" ,"Hayre","Jaggi","Hegde","Hora","Issac","Iyengar","Iyer","Jain","Jani","Jayaraman","Jha","Jhaveri","Johal","Joshi","Kadakia","Kade","Kakar","Kala","Kamdar"];
 
+
+export const LastNames = [ "Agarwal"," Agate","Agrawal"," Ahuja"," Amble"," Anand"," Andra"," Anne","Apte","Arora"," Arya","  Atwal"," Aurora"," Babu","Badal"," Badami","Bahl", " Bahri", "Bail","  Bains","  Bajaj","  Bajwa","Bakshi ",  "Bala", " Balan","Balay"," Bali"]
 export const commentsData = [
     {
         name:"aman",
