6d6af5bdce84b2c8236e025010036800ed3919db
Akash <akashbytes@gmail.com>
2023-10-04 16:01:11 +0530

    added liveChat slice in store.

--- src/RStore/UStore.js
+++ src/RStore/UStore.js
@@ -3,12 +3,14 @@ import NavToggle from "./ToggelSlice"
 import SearchCache from "./SearchCacheSlice"
 import SearchItems from "./SearchItemSlice"
 import YouTubeData from "./YouTubeDataSlice"
+import LiveChatSlice from "./LiveChatData"
 const AppStore = configureStore({
     reducer:{
      Toggle : NavToggle,
      SearchCacheData : SearchCache,
      SearchText_FromBtns :SearchItems,
      You_Tube_Data :YouTubeData,
+     LiveChatData : LiveChatSlice,
     }
 })
 
