b668451e1e8df4710e5bd4ae5de0ad0efd250dc7
Akash <akashbytes@gmail.com>
2023-10-11 12:45:53 +0530

    style added in component ...

--- src/Components/BodyComponents/Comments.js
+++ src/Components/BodyComponents/Comments.js
@@ -5,7 +5,7 @@ import{commentsData} from "../../Utils/UTAPIs"
 const CommentCard= ({data})=>{
     const {name,text} = data
     return(
-        <div className="h-[auto] w-[100%] flex bg-gray-200 m-1">
+        <div className="h-[auto] w-[100%] flex bg-slate-200 m-1 rounded-lg">
               <span className="text-2xl  px-3 py-2 m-1 rounded-full bg-gray-100 hover:bg-gray-200"><i className="fa-regular fa-circle-user"></i></span>
               <div className="px-2">
                 <p >{name}</p>
@@ -34,8 +34,8 @@ const CommentList = ({comments})=>{
 
 
 const CommentCounter = ()=>{
-    return(<div className="h-[auto] w-[100%] flex flex-col bg-gray-100 p-1 border border-violet-950">
-        <h2>Comments:</h2>
+    return(<div className="h-[auto] w-[100%] flex flex-col  p-1 border border-violet-950">
+        <h2 className="text-md font-semibold">Comments :</h2>
      
             <CommentList comments={commentsData}></CommentList>
     </div>)
