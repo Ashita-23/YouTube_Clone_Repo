3975f15673d5c820a2fa74b8e236b8731c1f0845
Akash <akashbytes@gmail.com>
2024-03-08 16:59:15 +0530

    suggesion video cards .....

---
+++ src/Components/suggestedCards.js
@@ -0,0 +1,39 @@
+
+import VideoCard from "./BodyComponents/VideoCard"
+import { useEffect, useState } from "react"
+import {YouTube_API} from "../Utils/UTAPIs"
+
+
+
+const SuggestedCards = ()=>{
+
+
+    const [data,setData]=useState([])
+console.log(data,"data")
+
+useEffect(()=>{
+    getUtubeData()
+
+  },[])
+
+  const  getUtubeData = async ()=> {
+   
+    const Utube_API = await fetch(YouTube_API);
+    const Json = await Utube_API.json()
+    // console.log(Json,"Json Data")
+    setData(Json.items)
+   
+  }
+
+
+    return(
+        <div>
+        {
+            data.map((data)=> <VideoCard data={data} key={data?.id} />)
+        }
+           
+        </div>
+    )
+}
+
+export default SuggestedCards
